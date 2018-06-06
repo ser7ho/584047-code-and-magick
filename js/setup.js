994e234ea464fdd44d25ff56d5bdfd4a91f7a57f
Ser7ho <serzzhh@gmail.com>
2018-06-06 10:49:04 +0300

    название констаты по стайлгайду

--- js/setup.js
+++ js/setup.js
@@ -4,8 +4,7 @@ var WIZARD_NAMES = ['Иван', 'Хуaн Себастьян', 'Мария', 'К�
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
-var wizardCount = 4;
-var wizards = [];
+var WIZARD_COUNT = 4;
 
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
